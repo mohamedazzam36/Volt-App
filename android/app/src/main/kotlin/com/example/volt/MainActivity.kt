0db8357b6a2580c7d9b0779af62e2b4e2dea2c18
mohamedazzam36 <memeazzam36@gmail.com>
2026-09-02 21:54:39 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/volt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.volt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:volt/volt_app.dart';
+
+void main() {
+  runApp(const VoltApp());
+}
