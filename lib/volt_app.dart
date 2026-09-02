24f1771b799ee7e5c64bfebb7848db9e9a2f5a06
mohamedazzam36 <memeazzam36@gmail.com>
2026-09-02 21:57:04 +0300

    chore: add .gitkeep to lib/core/utils directory for version control inclusion

---
+++ lib/volt_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VoltApp extends StatelessWidget {
+  const VoltApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
